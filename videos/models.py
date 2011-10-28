52ed1931c2f78e072d59f2adc1b5f7a3d97acf07
Yiğit Güneli <gro.tiiy@yiit.org>
2011-10-28 19:06:48 +0300

    adding filefields to according models

--- videos/models.py
+++ videos/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-# Create your models here.
 
 
 class Network(models.Model):
     """ About the network """
-
     name = models.CharField(max_length = 30, verbose_name = "Name of the Network") 
-    #@@TODO:
-    #logo = models.ImageField()
+    logo = models.ImageField(upload_to='logos')
 
     def __unicode__(self):
         return self.name
@@ -18,9 +14,8 @@ class Network(models.Model):
 class Channel(models.Model):
     """ About the Channel """
     network = models.ForeignKey(Network)
-    name = models.CharField(max_length = 20, verbose_name = "Name of the Channel")
-    #@@TODO:
-    #logo = models.ImageField()
+    name = models.CharField(max_length = 20, verbose_name = "Name of the Channel")    #@@TODO:
+    logo = models.ImageField(upload_to='logos')
 
     def __unicode__(self):
         return self.name
@@ -37,8 +32,7 @@ class Program(models.Model):
     """ About the Program """
     channel = models.ForeignKey(Channel)
     name = models.CharField(max_length = 40, verbose_name = "Name of the Program")
-    #@@TODO:
-    #    logo = models.ImageField()
+    logo = models.ImageField(upload_to='logos')
     category = models.ForeignKey(Category)
 
     def __unicode__(self):
@@ -49,13 +43,13 @@ class Video(models.Model):
     program = models.ForeignKey(Program)
     name = models.CharField(max_length = 20, verbose_name = "Name of the Video")
     episode_number = models.IntegerField(max_length=4, verbose_name = "Which episode in series")
-    #@@TODO: 
-    #thumbnail = models.ImageField()
+    thumbnail = models.ImageField(upload_to='thumbnails')
     pub_date = models.DateTimeField(auto_now = True,verbose_name = "Date of release")
     duration = models.IntegerField(max_length = 6, verbose_name = "Duration in seconds")
     view_count = models.IntegerField(max_length = 7, verbose_name = "Number of views")
     publisher = models.ForeignKey(User)
     last_watched = models.DateTimeField(verbose_name = "Last watched on")
+    video = models.FileField(upload_to='videos')
 
     def __unicode__(self):
         return self.name
