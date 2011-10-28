f2e54d23b537e7a299f9d65d10d793fdd2d70c73
Yiğit Güneli <gro.tiiy@yiit.org>
2011-10-28 20:03:43 +0300

    basic video view functionality

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,10 +8,16 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'kuzgun.views.home', name='home'),
     # url(r'^kuzgun/', include('kuzgun.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-                           url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-                           url(r'^admin/', include(admin.site.urls)),
+                          url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                          url(r'^admin/', include(admin.site.urls)),
+                          url(r'^watch/','watch.views.watch'),
 )
+
+
+# SERVING MEDIA_ROOT
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
--- watch/views.py
+++ watch/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from kuzgun.decorators import rendered_with
+from videos.models import Network
+from videos.models import Channel
+from videos.models import Category
+from videos.models import Program
+from videos.models import Video
+
+@rendered_with('watch/watch.html')
+def watch(request):
+	video = Video.objects.get()
+	return locals()
